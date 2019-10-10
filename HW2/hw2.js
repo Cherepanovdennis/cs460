191a753e7ad7a8932ebddb80ec4494384a069afc
Dennis <dcherepanov18@mail.wou.edu>
2019-10-10 12:47:10 -0700

    javascript now creates an ordered list and writes that into html producing a proper output! Html now needs to be formated and made pretty

--- HW2/hw2.js
+++ HW2/hw2.js
@@ -2,9 +2,25 @@
 
 
 function testfunction() {
-    var cash = document.getElementById("datacost").value;
-    var interest = document.getElementById("datasale").value;
-    document.getElementById("demo").innerHTML = cash
+    var cash = parseFloat(document.getElementById("datacost").value, 10); //convert input to int
+    var tax = parseFloat(document.getElementById("datasale").value,10) //convert input to int
+    var payment = parseFloat(document.getElementById("datapayment").value,10)
+	var totalprice = (cash * tax) + cash;
+	var totalstring = ""
+	
+	document.getElementById("demo").innerHTML = totalprice
+	while (totalprice > 0){
+		totalprice = totalprice - payment;
+		totalstring = totalstring + ("<li>" + (totalprice + "</li>")); 
+		
+		
+	}
+	// totalstring = totalstring + "</p>"
+     document.getElementById("demo").innerHTML = totalstring;
+	
+	
+	
+	
 }
 function myfunction() {
     
