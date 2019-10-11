3d730406bbafe8ab6d87158ba0e2973d48098e60
Dennis <dcherepanov18@mail.wou.edu>
2019-10-10 20:57:06 -0700

    finished the assignment, by adding .color, lu, and span in the .css file. I just need to make the video and merge to the master branch.

--- HW2/hw2.js
+++ HW2/hw2.js
@@ -3,15 +3,16 @@
 
 function testfunction() {
     var cash = parseFloat(document.getElementById("datacost").value, 10); //convert input to int
-    var tax = parseFloat(document.getElementById("datasale").value,10) //convert input to int
-    var payment = parseFloat(document.getElementById("datapayment").value,10)
+    var tax = parseFloat(document.getElementById("datasale").value, 10); //convert input to int
+    var payment = parseFloat(document.getElementById("datapayment").value, 10);
 	var totalprice = (cash * tax) + cash;
-	var totalstring = ""
-	
+    var totalstring = "";
+
+
 	document.getElementById("demo").innerHTML = totalprice
 	while (totalprice > 0){
-		totalprice = totalprice - payment;
-		totalstring = totalstring + ("<li>" + (totalprice + "</li>")); 
+        totalprice = totalprice - payment;
+        totalstring = totalstring +  ("<li>" + totalprice + "</li>"); 
 		
 		
 	}
@@ -19,9 +20,4 @@ function testfunction() {
      document.getElementById("demo").innerHTML = totalstring;
 	
 	
-	
-	
-}
-function myfunction() {
-    
 }
