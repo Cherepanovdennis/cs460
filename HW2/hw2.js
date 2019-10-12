5bce9c9b253db63478c2d32b76554b66442e2973
Dennis <dcherepanov18@mail.wou.edu>
2019-10-11 20:38:14 -0700

    added safeguards in the javascript file that will account if the user puts in a string or non-int/float into the input

--- HW2/hw2.js
+++ HW2/hw2.js
@@ -2,22 +2,38 @@
 
 
 function testfunction() {
-    var cash = parseFloat(document.getElementById("datacost").value, 10); //convert input to int
-    var tax = parseFloat(document.getElementById("datasale").value, 10); //convert input to int
+    var cash = parseFloat(document.getElementById("datacost").value, 10); //convert input to float
+    var tax = parseFloat(document.getElementById("datasale").value, 10); //convert input to float
     var payment = parseFloat(document.getElementById("datapayment").value, 10);
 	var totalprice = (cash * tax) + cash;
     var totalstring = "";
-
-
+	var month = 0;
+	
+	if ( isNaN(cash) == true || isNaN(tax) == true || isNaN(payment) == true){
+		document.getElementById("demo").innerHTML = "<li> Please put in an int or floating point number </li> "
+	}
+	
+    else {
 	document.getElementById("demo").innerHTML = totalprice
 	while (totalprice > 0){
         totalprice = totalprice - payment;
-        totalstring = totalstring +  ("<li>" + totalprice + "</li>"); 
+		month++;
+		
+		if(totalprice > 0){
+		totalstring = totalstring +  ("<li> Month " + month + " your current balance is: " + totalprice + "$" + "</li>"); }
+		
+		else{
+		totalstring = totalstring +  ("<li> Month " + month + " your current balance is: " + 0 + "$" + "</li>");}
 		
 		
 	}
+	totalstring = totalstring + "<li> You'll pay off your car in " + month + " months! </li>";
 	// totalstring = totalstring + "</p>"
      document.getElementById("demo").innerHTML = totalstring;
 	
-	
+}	
+}
+
+function isInt(x){
+return x % 1 === 0;
 }
