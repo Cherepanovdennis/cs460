4470aa63ee94cf5120b91dc6a77edcce82490707
Dennis <dcherepanov18@mail.wou.edu>
2019-10-16 17:27:05 -0700

    updated node.cs and created QueueInterface

--- HW3/main/Hw3/Node.cs
+++ HW3/main/Hw3/Node.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Hw3
 {
-    class Node
+    public class Node<T>
     {
+        public T data;
+        public Node<T> next;
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
     }
 }
--- HW3/main/Hw3/Program.cs
+++ HW3/main/Hw3/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Hw3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
