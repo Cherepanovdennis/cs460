ea855a9df4011a4bec63199eabf10d51cc73adae
Dennis <dcherepanov18@mail.wou.edu>
2019-10-16 18:03:47 -0700

    Created QueueInterface, there is no throws equivalent in C# so im not sure what to do

---
+++ HW3/main/Hw3/QueueInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw3
+{
+    public interface QueueInterface<T>
+    {
+        T push(T element);
+        T pop(); // there are no throw exceptions in C#
+        T peek(); // there are not throw exceptions in C#
+        bool isEmpty();
+
+
+    }
+}
