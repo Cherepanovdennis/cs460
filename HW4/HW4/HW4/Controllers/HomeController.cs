28b515741f84f959ed451ac10da86daf93e8a5d9
Dennis <dcherepanov18@mail.wou.edu>
2019-10-24 16:54:54 -0700

    Connected Home to Rgb color and did some formatting

--- HW4/HW4/HW4/Controllers/HomeController.cs
+++ HW4/HW4/HW4/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HW4.Models;
+using System.Drawing;
 
 namespace HW4.Controllers
 {
@@ -27,6 +28,30 @@ namespace HW4.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult RGBColor()
+        {
+            
+                if (Request.Query["red"].ToString() == "")
+                {
+                    ViewBag.Success = false;
+                    return View();
+                }
+                else
+                {
+                    string ColorRed = Request.Query["red"].ToString();
+                    string colorblue = Request.Query["blue"].ToString();
+                    string colorgreen = Request.Query["green"].ToString();
+                    ViewBag.RedRGB = System.Convert.ToInt32(ColorRed);
+                    ViewBag.BlueRGB = System.Convert.ToInt32(colorblue);
+                    ViewBag.GreenRGB = System.Convert.ToInt32(colorgreen);
+                    Color mycolor = Color.FromArgb(System.Convert.ToInt32(ColorRed), System.Convert.ToInt32(colorgreen), System.Convert.ToInt32(colorblue));
+                    ViewBag.Hex = mycolor.R.ToString("X2") + mycolor.G.ToString("X2") + mycolor.B.ToString("X2");
+                    ViewBag.Success = true;
+                    return View();
+                }
+            }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
