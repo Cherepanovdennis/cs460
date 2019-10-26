a776d58ff38874308bcbe5646b22e7b666d18ff0
Dennis <dcherepanov18@mail.wou.edu>
2019-10-25 18:09:46 -0700

    started working on the RGB color table

---
+++ HW4/HW4/HW4/Controllers/RGBTableController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW4.Controllers
+{
+    public class RGBTableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Hexred, string Hexgreen, string Hexblue)
+        {
+            return View();
+        }
+    }
+}
