d1b9e00348d1cc6d0176c0ad2340c6b4df82677a
Dennis <dcherepanov18@mail.wou.edu>
2019-10-31 13:36:18 -0700

    I accidently used a core framwork instead of a .net framwork. I fixed it and intend to delete the old homework once I get everything figured out

---
+++ HW5/NewHW5/NewHW5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewHW5.MvcApplication" Language="C#" %>
---
+++ HW5/NewHW5/NewHW5/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NewHW5.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required, DisplayName("First Name")]
+        [StringLength(64)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Date of Birth"), Required]
+        public DateTime DOB { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                var age = today.Year - DOB.Year;
+                if (DOB > today.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {FirstName} {LastName} DOB = {DOB} Age = {Age}";
+        }
+    }
+}
