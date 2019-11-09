ac21977bfced7b9d09d9488946d8836694940ea4
Dennis <dcherepanov18@mail.wou.edu>
2019-11-08 17:39:54 -0800

    Attempted to covert Customfield into a json so that I can access it, did not work.

--- HW6/HW6/HW6/Models/ViewModel/StockItemModelView.cs
+++ HW6/HW6/HW6/Models/ViewModel/StockItemModelView.cs
@@ -3,33 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace HW6.Models.ViewModel
 {
     public class StockItemModelView
     {
-        public StockItemModelView(StockItem stockItem)
+        public StockItemModelView(System.Linq.IQueryable<StockItem> stockItem)
         {
 
+            foreach (StockItem Item in stockItem) {
+                StockItemName.Add(Item.StockItemName);
+                ItemSize.Add(Item.Size);
+                ReccomendedPrice.Add(Item.RecommendedRetailPrice);
+                ProductWeight.Add(Item.TypicalWeightPerUnit);
+                LeadTimeDays.Add(Item.LeadTimeDays);
+                ValidSince.Add(Item.ValidFrom);
+                JObject country = Item.CustomFields;
+                CountryOrigin.Add(country);
+
+
+
+                    }
 
 
         }
-        public string StockItemName { get; private set; }
+        public List<string> StockItemName { get; private set; }
 
-        public string ItemSize { get; private set; }
+        public List<string> ItemSize { get; private set; }
 
-        public double ReccomendedPrice { get; private set; }
+        public List<decimal?> ReccomendedPrice { get; private set; }
 
-        public double ProductWeight { get; private set; }
+        public List<decimal> ProductWeight { get; private set; }
 
-        public int LeadTimeDays { get; private set; }
+        public List<int> LeadTimeDays { get; private set; }
 
-        public DateTime ValidSince { get; private set; }
+        public List<DateTime> ValidSince { get; private set; }
 
-        public string CountryOrigin { get; private set; }
+        public List<string> CountryOrigin { get; private set; }
 
-        public string[] Tags { get; private set; }
+        public List<string[]> Tags { get; private set; }
 
-        public byte[] Photo { get; private set; }
+        public List<byte[]> Photo { get; private set; }
 
 
 
