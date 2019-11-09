63690fc1bed04c9c247a62cd5c079db46646b961
Dennis <dcherepanov18@mail.wou.edu>
2019-11-08 16:59:28 -0800

    Added search controller and viewmodel StockItemModel, worked on controller.

---
+++ HW6/HW6/HW6/Controllers/SearchController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HW6.DAL;
+using HW6.Models;
+using HW6.Models.ViewModel;
+
+
+namespace HW6.Controllers
+{
+    public class SearchController : Controller
+    {
+        private WideWorldImportersContext db = new WideWorldImportersContext();
+        // GET: Search
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string ProductName)
+        {
+            if(string.IsNullOrEmpty(ProductName) == true)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+           System.Linq.IQueryable<StockItem> stockItem = db.StockItems.Where(Item => Item.StockItemName.Contains(ProductName));
+
+            if(stockItem.Count)
+            {
+                return HttpNotFound();
+            }
+
+            System.Linq.IQueryable<StockItemModelView> StockItemTable = new System.Linq.IQueryable<StockItemModelView>(stockItem);
+
+        }
+    }
+}
---
+++ HW6/HW6/HW6/Models/ViewModel/StockItemModelView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW6.Models.ViewModel
+{
+    public class StockItemModelView
+    {
+        public StockItemModelView(StockItem stockItem)
+        {
+
+
+
+        }
+        public string StockItemName { get; private set; }
+
+        public string ItemSize { get; private set; }
+
+        public double ReccomendedPrice { get; private set; }
+
+        public double ProductWeight { get; private set; }
+
+        public int LeadTimeDays { get; private set; }
+
+        public DateTime ValidSince { get; private set; }
+
+        public string CountryOrigin { get; private set; }
+
+        public string[] Tags { get; private set; }
+
+        public byte[] Photo { get; private set; }
+
+
+
+    }
+}
