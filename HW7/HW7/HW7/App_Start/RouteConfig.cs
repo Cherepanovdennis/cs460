9c34168ba30d1679d2d3b345fc4e811701de2305
Dennis <dcherepanov18@mail.wou.edu>
2019-11-19 14:20:44 -0800

    Finished assignment by creating commit tables. I need to create a video and then submit the coursework

--- HW7/HW7/HW7/App_Start/RouteConfig.cs
+++ HW7/HW7/HW7/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace HW7
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "CustomRoute",
+                url: "api/commits/{id}",
+                defaults: new {Controller = "Home", action = "CommitHistory", id = UrlParameter.Optional}
+                ); 
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- HW7/HW7/HW7/Scripts/main.js
+++ HW7/HW7/HW7/Scripts/main.js
@@ -7,9 +7,9 @@ function commitFun(id) {
     $.ajax({
         type: "GET",
         dataType: "json",
-        url: "/Home/CommitHistory?user=" + user + "&repo=" + Repository,
+        url: "/api/commits?user=" + user + "&repo=" + Repository,
         data: { 'userName': user, 'repoName': Repository },
-        success: displayCommits,
+        success: Commits,
         error: errorOnAjax
     });
 };
@@ -19,4 +19,34 @@ function errorOnAjax() {
     console.log('Error on AJAX return');
 }
 
+function Commits(info) {
+    $("#newtable").empty();
+    var table = document.getElementById("newtable");
+    var header = table.createTHead();
+    var row = header.insertRow(0);
+    var cell = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
+    cell.innerHTML = "<b>SHA</b>";
+    cell1.innerHTML = "<b>TimeStamp</b>";
+    cell2.innerHTML = "<b>Commiter</b>";
+    cell3.innerHTML = "<b>Message</b>";
+
+    for (var i = info.length - 1; i != 0; i--) {
+        var insertrow = 1;
+        var row = table.insertRow(insertrow);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.innerHTML = '<a href="' + info[i].CommitUri + '">' + info[i].Hash + '</a>';
+        cell2.innerHTML = info[i].CommitDateTime;
+        cell3.innerHTML = info[i].WhoCommited;
+        cell4.innerHTML = info[i].CommitMessage;
+        insertrow++;
+
+    }
+
+}
 
