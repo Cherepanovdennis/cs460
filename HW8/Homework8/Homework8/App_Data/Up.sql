7c3a2d01249bf1085e2a108cb763e6113583c98e
Dennis <dcherepanov18@mail.wou.edu>
2019-11-28 19:43:06 -0800

    Updated seed and added a timeslot to athlete table

--- HW8/Homework8/Homework8/App_Data/Up.sql
+++ HW8/Homework8/Homework8/App_Data/Up.sql
@@ -17,10 +17,13 @@ CoachID int NOT NULL REFERENCES Coaches(CoachID)
 
 
 CREATE TABLE Athletes(
-  AthleteId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  AthleteId int IDENTITY(1,1) NOT NULL,
   AthleteName nvarchar(255) NOT NULL,
   AthleteGender nvarchar(200) NOT NULL Check (AthleteGender = 'Boys' OR AthleteGender = 'Girls'),
   TeamID int NOT NULL REFERENCES Teams(TeamID),
   CoachID INT NOT NULL REFERENCES Coaches(CoachID),
-  EventID INT NOT NULL REFERENCES RaceEvents(EventID)
+  EventID INT NOT NULL REFERENCES RaceEvents(EventID),
+  RaceTime REAL
 );
+ALTER TABLE Athletes
+    ADD CONSTRAINT pk_myConstraint PRIMARY KEY (AthleteID,EventID);
